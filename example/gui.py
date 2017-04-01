df4819c0e48e19d4299db945cbb0da024de82e23
mxgsa <mxgsa@vip.qq.com>
2017-04-01 14:42:32 +0800

    Update gui.py

--- example/gui.py
+++ example/gui.py
@@ -14,16 +14,53 @@ __author__ = 'mengxg'
 from Tkinter import *
 import tkMessageBox
 
-'''
-简单的实例可以这样写
 
-root=Tk()
-frame=Frame(root, width=512, height=512)
-frame.pack()
-w=Label(root, text='Hello, world!')
-w.pack()
-root.mainloop()
-'''
+# 简单的实例可以这样写
+
+# 使用Tkinter进行GUI编程
+def tkinter_demo():
+    # 创建一个主窗口
+    root=Tk()
+    # 设置主窗口的初始大小
+    root.geometry('300x300')
+    # 创建容器，负责安排其他部件的位置
+    frame=Frame(root)
+    frame.pack()
+    # 创建一个输入框
+    helloText=Entry(root)
+    helloText.pack()
+    # 按钮点击事件
+    def ok_click():
+        txt = helloText.get() or 'world'
+        tkMessageBox.showinfo('Message', 'Hello, %s' % txt)
+    w=Label(root, text='Hello, world!')
+    w.pack()
+    # 创建一个按钮
+    button=Button(root ,text= 'OK',command = ok_click)
+    button.pack()
+    # 进入主事件循环
+    root.mainloop()
+
+import wx
+class MyFrame(wx.Frame):
+	def __init__(self):
+		wx.Frame.__init__(self, None, -1, "My Frame", size = (300, 300))
+		panel = wx.Panel(self, -1)
+		panel.Bind(wx.EVT_MOTION, self.OnMove)
+		wx.StaticText(panel, -1, "Pos:", pos = (10, 12))
+		self.posCtrl = wx.TextCtrl(panel, -1, "", pos = (40, 10))
+
+	def OnMove(self, event):
+		pos = event.GetPosition()
+		self.posCtrl.SetValue("%s, %s" %(pos.x, pos.y))
+if __name__ == '__main__':
+	app = wx.PySimpleApp()
+	frame = MyFrame()
+	frame.Show(True)
+	app.MainLoop()
+
+
+
 
 
 # 大的一些项目可以这样来写，封装到一个类里面
